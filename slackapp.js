1751921c3948e70d8cf8305ad2c5c5fa58eae3cb
Matt Doering <mattdoering86@gmail.com>
2022-08-26 11:43:54 -0400

    added a fetch function to send a message event Kustomer

--- slackapp.js
+++ slackapp.js
@@ -14,11 +14,22 @@ const Kusty = {
   }
 
 /* Add functionality here */
+const sendToKustomer = (body, token) => {
+    console.log(body)
+      return fetch('https://api.kustomerapp.com/v1/hooks/form/622fb8c637c9d9f46c7389d6/0c0bc9c9b450764228dc68364d70592979d4d67a78f16f163037cf32decf3f65', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+          body: body && JSON.stringify(body)
+        })
+          .then(resp => resp.json())
+  }
+
 app.event('app_mention', ({ event, say }) => {
     console.log(event)
 
     
   });
+
 
 
 (async () => {
