6113b767a1af4a1bc8019508468bfc398f371889
Matt Doering <mattdoering86@gmail.com>
2022-08-26 09:17:29 -0400

    updated start of the app

--- slackapp.js
+++ slackapp.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const { App } = require('@slack/bolt');
 
+
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
@@ -8,9 +9,16 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN
 });
 
+const Kusty = {
+    token: process.env.KUSTOMER_API_TOKEN
+  }
+
 /* Add functionality here */
+app.event('app_mention', ({ event, say }) => {
+    console.log(event)
 
-
+    
+  });
 
 
 (async () => {
